02902cdd0ab47e25220dde6ba1003a798164879f
Constantin Ungureanu <constantin.ungureanu.github@gmail.com>
2018-03-02 19:56:17 +0100

    Updated the base app.

--- src/main/java/com/masterminds/DemoServlet.java
+++ src/main/java/com/masterminds/DemoServlet.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.masterminds;
 
 import java.io.IOException;
@@ -27,16 +11,17 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class DemoServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Override
-    public void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain");
+  @Override
+  public void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws
+      IOException {
+    resp.setContentType("text/plain");
 
-        try (final PrintWriter writer = resp.getWriter()) {
-            writer.println("{ \"name\": \"World\" }");
-        } catch (final Exception e) {
-            log.error("{}", e.getMessage(), e);
-        }
+    try (final PrintWriter writer = resp.getWriter()) {
+      writer.println("{ \"name\": \"Masterminds World!\" }");
+    } catch (final Exception e) {
+      log.error("{}", e.getMessage(), e);
     }
+  }
 }
